e257bf91042c343419f3bff88d8b10de5b26a0a0
asu <arsenyk.ustaris.dev@gmail.com>
2015-08-31 14:02:37 -0700

    Initial Commit

---
+++ MathContest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathContest
+//
+//  Created by asu on 2015-08-31.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MathContest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MathContest
+//
+//  Created by asu on 2015-08-31.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
